9d7abe8e3133c4e5cfb8192eea07f3ff286ed0d3
k2soft <kbsoft@onet.eu>
2018-11-19 14:13:40 +0100

    test2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp5
 {
@@ -10,6 +12,81 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+
+            string kdkd=null;
+
+ 
+            string xmlString = "<Products><Product><Id>1</Id><Name>My XML product</Name></Product><Product><Id>2</Id><Name>My second product</Name></Product></Products>";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Product>), new XmlRootAttribute("Products"));
+
+            StringReader stringReader = new StringReader(xmlString);
+
+            
+            List<Product> productList = (List<Product>)serializer.Deserialize(stringReader);
+
+            string outxml = "";
+
+
+
+            serializer = new XmlSerializer(typeof(List<Product>),new XmlRootAttribute("Products"));
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                serializer.Serialize(textWriter, productList);
+                outxml= textWriter.ToString();
+            }
+
+            XmlSerializer serializer1 = new XmlSerializer(typeof(List<Product>), new XmlRootAttribute("Products"));
+
+            StringReader stringReader1 = new StringReader(outxml);
+
+
+            List<Product> productList1 = (List<Product>)serializer1.Deserialize(stringReader1);
+
         }
+
+
     }
+
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [System.Xml.Serialization.XmlElement(IsNullable = true)]
+        public string Name2 { get; set; }
+        public int Id2 { get; set; }
+
+
+        public void Save(string path)
+        {
+            var serializer = new XmlSerializer(typeof(Product));
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }
+
+        public string Save()
+        {
+            var serializer = new XmlSerializer(typeof(Product));
+            using (StringWriter textWriter = new StringWriter())
+            {
+                serializer.Serialize(textWriter, this);
+                return textWriter.ToString();
+            }
+
+
+            //using (var stream = new MemoryStream())
+            //{
+            //    serializer.Serialize(stream, this);
+            //}
+        }
+
+
+
+    }
+
+
 }
