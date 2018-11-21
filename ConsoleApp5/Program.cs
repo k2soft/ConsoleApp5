91e798fa4b3133cb824b88dfa5e457cf58c3d8c3
k2soft <kbsoft@onet.eu>
2018-11-21 13:43:17 +0100

    zmiana3a

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleApp5
 
             Product pr = new Product() { Name = "test" };
             Product pr1 = new Product();
-            var dres = new { pasek1 = "biały", pasek2 = "czarny" };
+            var dres = new { pasek1 = "biały", pasek2 = "czarny", pasek3 = "niebieski" };
 
             string vhsh = pr?.Name ?? "e";
             bool fName = true;
