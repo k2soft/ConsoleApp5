fab332d5c8a4928b71b52cbe5c80f533b783a6fe
k2soft <kbsoft@onet.eu>
2018-11-21 13:02:57 +0100

    testy 2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -13,9 +13,23 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
 
-            string kdkd=null;
+            string kdkd = null;
 
- 
+            int? s = null; //a, teraz ok
+
+            Product pr = new Product() { Name = "test" };
+            Product pr1 = new Product();
+            var dres = new { pasek1 = "biały" };
+
+            string vhsh = pr?.Name ?? "e";
+            bool fName = true;
+
+            string address = pr?.Name?.ToString();
+
+            string first = "Jon", last = "Snow";
+            var s_csharp6 = $"{first} likes {last} now {DateTime.Now:d}";
+            var dic = new Dictionary<string, int> { ["x"] = 1, ["y"] = 2, };
+
             string xmlString = "<Products><Product><Id>1</Id><Name>My XML product</Name></Product><Product><Id>2</Id><Name>My second product</Name></Product></Products>";
 
             XmlSerializer serializer = new XmlSerializer(typeof(List<Product>), new XmlRootAttribute("Products"));
@@ -55,9 +69,9 @@ namespace ConsoleApp5
         public int Id { get; set; }
         public string Name { get; set; }
         [System.Xml.Serialization.XmlElement(IsNullable = true)]
-        public string Name2 { get; set; }
+        public string Name2 { get; set; } = "Marian";
         public int Id2 { get; set; }
-
+        public bool IsAdmin { get; set; }
 
      }
 
